f87c11ef61b3d2ead703b389c4e8c15a9723c63f
Waterticket <matthew218@naver.com>
2022-12-28 14:52:23 +0900

    mysql 추가

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module youtube-demo
 
 go 1.19
 
-require github.com/labstack/echo/v4 v4.10.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/labstack/echo/v4 v4.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/labstack/echo/v4"
+
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	db *sql.DB
+	e  *echo.Echo
 )
 
 func main() {
-	e := echo.New()
+	var err error
+	db, err = sql.Open("mysql", "root:password@tcp(127.0.0.1:3307)/ytdemo")
+	defer db.Close()
+
+	if err != nil {
+		fmt.Println("MYSQL Connect Failed: ", err)
+		os.Exit(0)
+	}
+
+	var version string
+	db.QueryRow("SELECT VERSION()").Scan(&version)
+	fmt.Println("Connected to:", version)
+
+	e = echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
+
+	sig := make(chan os.Signal)
+	signal.Notify(sig, os.Interrupt)
+
+forever:
+	for {
+		select {
+		case <-sig:
+			fmt.Println("signal received, stopping")
+			break forever
+		}
+	}
 }
